d6ca795ed2ca3026d2822b7fdb1e4474fdf62606
Pyth-n <19356494+Pyth-n@users.noreply.github.com>
2022-01-28 14:12:51 -0800

    Update App.js
    create Part compnent

--- part1/course-information/src/App.js
+++ part1/course-information/src/App.js
@@ -11,14 +11,18 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <>
+      <Part part={props.part[0]} exercise={props.exercises[0]} />
+      <Part part={props.part[1]} exercise={props.exercises[1]} />
+      <Part part={props.part[2]} exercise={props.exercises[2]} />
+    </>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <>
       <p>
-        {props.part[0]}: {props.exercises[0]}
-      </p>
-      <p>
-        {props.part[1]}: {props.exercises[1]}
-      </p>
-      <p>
-        {props.part[2]}: {props.exercises[2]}
+        {props.part}: {props.exercise}
       </p>
     </>
   )
