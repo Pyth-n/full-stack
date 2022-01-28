aeceb4ee70426943d2efd91b7fecec80f199cd7f
Pyth-n <19356494+Pyth-n@users.noreply.github.com>
2022-01-28 13:36:12 -0800

    add basic course information

--- part1/course-information/src/App.js
+++ part1/course-information/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises = 10
+  const part2 = 'Using props to pass data'
+  const exercise2 = 7
+  const part3 = 'State of a component'
+  const exercise3 = 14
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{course}</h1>
+      <p>
+        {part1} {exercises}
+      </p>
+      <h1>{course}</h1>
+      <p>
+        {part2} {exercise2}
+      </p>
+      <h1>{course}</h1>
+      <p>
+        {part3} {exercise3}
+      </p>
     </div>
   );
 }
