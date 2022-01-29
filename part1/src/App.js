160a94646ca1d31ca67f07d297477ddfa532fcf8
Pyth-n <19356494+Pyth-n@users.noreply.github.com>
2022-01-28 18:47:12 -0800

    Update App.js
    concat arrays

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,19 +1,27 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
+  const [ left, setLeft ] = useState(0)
+  const [ right, setRight ] = useState(0)
+  const [ allClicks, setAll ] = useState([])
 
-  const increaseByOne = () => setCounter(counter + 1)
-  const decreaseByOne = () => setCounter(counter - 1)
-  const reset = () => setCounter(0)
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
 
   return(
     <div>
-      <Display counter={counter} />
-      <br />
-      <Button onClick={decreaseByOne} text='minus' />
-      <Button onClick={reset} text='reset' />
-      <Button onClick={increaseByOne} text='plus' />
+      {left}
+      <Button onClick={handleLeftClick} text='left' />
+      <Button onClick={handleRightClick} text='right' />
+      {right}
+      <p>{allClicks.join(' ')}</p>
     </div>
   )
 }
