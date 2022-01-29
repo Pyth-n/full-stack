380d1e4ebc092026bb676efd010717d9c6b80322
Pyth-n <19356494+Pyth-n@users.noreply.github.com>
2022-01-28 17:57:58 -0800

    Update App.js
    create display component

--- part1/src/App.js
+++ part1/src/App.js
@@ -4,16 +4,24 @@ const App = () => {
   const [ counter, setCounter ] = useState(0)
 
   const increaseByOne = () => setCounter(counter + 1)
-  const decreaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
   const reset = () => setCounter(0)
 
   return(
     <div>
-      {counter}
+      <Display counter={counter} />
       <br />
       <button onClick={decreaseByOne}>minus</button>
       <button onClick={reset}>reset</button>
       <button onClick={increaseByOne}>plus</button>
+    </div>
+  )
+}
+
+const Display = ({ counter }) => {
+  return (
+    <div>
+      {counter}
     </div>
   )
 }
