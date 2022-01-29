67db2c698004ca5ae9238318524e3b53603466f2
Pyth-n <19356494+Pyth-n@users.noreply.github.com>
2022-01-28 16:12:22 -0800

    Update App.js
    create part objects

--- part1/course-information/src/App.js
+++ part1/course-information/src/App.js
@@ -9,11 +9,12 @@ const Header = (props) => {
 }
 
 const Content = (props) => {
+  const part = props.part
   return (
     <>
-      <Part part={props.part[0]} exercise={props.exercises[0]} />
-      <Part part={props.part[1]} exercise={props.exercises[1]} />
-      <Part part={props.part[2]} exercise={props.exercises[2]} />
+      <Part part={part[0].name} exercise={part[0].exercises} />
+      <Part part={part[1].name} exercise={part[1].exercises} />
+      <Part part={part[2].name} exercise={part[2].exercises} />
     </>
   )
 }
@@ -45,18 +46,24 @@ const Total = (props) => {
 function App() {
   const course = 'Half Stack application development'
 
-  const part1 = 'Fundamentals of React'
-  const exercise1 = 10
-  const part2 = 'Using props to pass data'
-  const exercise2 = 7
-  const part3 = 'State of a component'
-  const exercise3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course}/>
-      <Content part={[part1, part2, part3]} exercises={[exercise1, exercise2, exercise3]} />
-      <Total exercises={[exercise1, exercise2, exercise3]} />
+      <Content part={[part1, part2, part3]}/>
+      {/* <Total exercises={[exercise1, exercise2, exercise3]} /> */}
     </div>
   );
 }
