5e8e3d8bd1af3ca4e9191695b1b41052dc3f5148
Pyth-n <19356494+Pyth-n@users.noreply.github.com>
2022-01-29 17:32:58 -0800

    create
    simple note app

---
+++ part2/src/App.js
@@ -0,0 +1,15 @@
+
+function App(props) {
+  const { notes } = props
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => <li key={note.id}>{note.content}</li>)}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
