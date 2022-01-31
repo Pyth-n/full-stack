1677181a7bb8084ea4a50fbc360e961d1bde8aa6
Pyth-n <19356494+Pyth-n@users.noreply.github.com>
2022-01-31 15:07:21 -0800

    Update App.js
    course becomes array, fix header

--- part1/course-information/src/App.js
+++ part1/course-information/src/App.js
@@ -1,9 +1,10 @@
-const Header = (props) => {
+const Header = ({ courses }) => {
+  console.log(courses)
   return (
     <>
-      <h1>
-        {props.course}
-      </h1>
+        {courses.map(x => 
+          <h1>{x.toString()}</h1>
+        )}
     </>
   )
 }
@@ -31,7 +32,7 @@ const Part = (props) => {
 
 const Total = ({ parts }) => {
   let total = parts.reduce((t, c) => t + c.exercises, 0)
-  
+
   return (
     <>
       <strong>
@@ -44,37 +45,63 @@ const Total = ({ parts }) => {
 }
 
 function App() {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const course = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return <Course course={course} />
 }
 
 const Course = ({ course }) => {
+  let names = course.map(x => x.name)
+  let parts = course.map(x => x.parts)
+  
   return (
     <div>
-      <Header course={course.name}/>
-      <Content part={course.parts}/>
-      <Total parts={course.parts} />
+      {<Header courses={names}/>
+      /*<Content part={course.parts}/>
+      <Total parts={course.parts} /> */}
     </div>
   );
 }
