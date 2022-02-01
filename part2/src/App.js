9cf86ea39e210b1f11fa7a227906190e4b53b351
Pyth-n <19356494+Pyth-n@users.noreply.github.com>
2022-01-31 17:47:59 -0800

    Create input and submit button for new note

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,15 +1,31 @@
+import { useState } from 'react'
 import Note from "./components/Note";
 
-function App({ notes }) {
+function App({ data }) {
+  const [notes, setNotes] = useState(data)
+  const [newNote, setNewNote] = useState('new note...')
 
+  const addNote = (event) => {
+    event.preventDefault()
+    console.log('button clicked', event.target)
+  }
+
+  const handleNewNote = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        {notes.map(note => 
-          <Note key={note.id} content={note.content} />
+        {data.map(data => 
+          <Note key={data.id} content={data.content} />
         )}
       </ul>
+      <form onSubmit={addNote}>
+          <input value={newNote} onChange={handleNewNote} />
+          <button type='submit'>save</button>
+      </form>
     </div>
   );
 }
--- part2/src/index.js
+++ part2/src/index.js
@@ -25,7 +25,7 @@ const notes = [
 
 ReactDOM.render(
   <React.StrictMode>
-    <App notes={notes}/>
+    <App data={notes}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
