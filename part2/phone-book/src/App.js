67eca61c740fa0d7035515f9f68a66304a1dbbac
Pyth-n <19356494+Pyth-n@users.noreply.github.com>
2022-01-31 18:18:05 -0800

    Update App.js
    prototype begin

--- part2/phone-book/src/App.js
+++ part2/phone-book/src/App.js
@@ -1,8 +1,32 @@
+import { useState } from 'react'
 
 function App() {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [newName, setNewName] = useState('Ada Lovelace')
+
+  const handleNewName = (event) => {
+    setNewName(event.target.value)
+  }
+
   return (
     <div>
-      
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input value={newName} onChange={handleNewName} />
+        </div>
+        <div>
+          <button type='submit'>
+            add
+          </button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        debug: {newName}
+      </div>
     </div>
   );
 }
