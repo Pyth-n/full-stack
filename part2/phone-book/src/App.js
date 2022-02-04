ea46e30f773265c518e68637775cb611e2c6ec19
Pyth-n <19356494+Pyth-n@users.noreply.github.com>
2022-02-04 13:29:04 -0800

    Update App.js
    create success component

--- part2/phone-book/src/App.js
+++ part2/phone-book/src/App.js
@@ -90,4 +90,13 @@ function App() {
   );
 }
 
+const Success = ({ message }) => {
+  if (message === null) return null
+  return(
+    <div className='success'>
+      {message}
+    </div>
+  )
+}
+
 export default App;
