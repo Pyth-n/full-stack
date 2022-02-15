f430c65781cce7704e3d1e9f060dd6a601664958
Pyth-n <19356494+Pyth-n@users.noreply.github.com>
2022-02-14 20:07:25 -0800

    Update index.js

--- backend/phonebook-backend/index.js
+++ backend/phonebook-backend/index.js
@@ -2,6 +2,32 @@ const express = require('express')
 const app = express()
 const PORT = 3001
 
-app.get('/', (req, res) => res.send('hola'))
+const data = [
+  { 
+    "id": 1,
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": 2,
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": 3,
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": 4,
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
+]
+
+app.get('/info', (req, res) => {
+  res.send(`Phonebook has info for ${data.length} people <br /><br />${Date()}`)
+})
+app.get('/api/persons', (req, res) => res.json(data))
 
 app.listen(PORT, (req, res) => console.log(`listening on port ${PORT}`))
