80b05f1ad082efc298889864cbbef013a1353c29
Pyth-n <19356494+Pyth-n@users.noreply.github.com>
2022-02-14 19:54:13 -0800

    Update index.js
    hard code

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let notes = [
   {
     id: 1,
@@ -22,8 +24,32 @@ let notes = [
   }
 ]
 
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id))
+  : 0
+  return maxId + 1
+}
+
 app.get('/', (req, res) => res.send('<h1>hello world</h1>'))
 app.get('/api/notes', (req, res) => res.json(notes))
+app.post('/api/notes', (req, res) => {
+  const body = req.body
+
+  if (!body.content) {
+    return res.status(400).json({
+      error: 'content missing'
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId()
+  }
+  notes = notes.concat(note)
+  res.json(note)
+})
 
 app.get('/api/notes/:id', (req, res) => {
   const id = Number(req.params.id)
