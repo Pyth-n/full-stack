b2f1f96058b23cc4c740d9d5ea9d0921557696ed
Pyth-n <19356494+Pyth-n@users.noreply.github.com>
2022-02-16 14:21:12 -0800

    fix api

--- backend/phonebook-backend/index.js
+++ backend/phonebook-backend/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const morgan = require('morgan')
 const PORT = 3001
 
+app.use(cors())
 app.use(express.json())
 
 morgan.token('body', (req, res) => JSON.stringify(req.body))
